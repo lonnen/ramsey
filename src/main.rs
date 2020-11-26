8de445fea76bbc3ad3a31901b17c8da8d0d56290
lonnen <chris.lonnen@gmail.com>
2020-11-25 22:35:28 -0800

    basic command line parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Opt {
+
+    #[structopt(short, long)]
+    debug: bool,
+
+    #[structopt(short, long)]
+    verbose: bool,
+
+
+
+    pattern: String,
+    #[structopt(parse(from_os_str))]
+    path: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Opt::from_args();
 }
